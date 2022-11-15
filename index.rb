ecbbb5b81446a036c17eb284fa9f07fd43d02f41
EzequielOmar <ezequiel.omar.vazquez@gmail.com>
2022-11-15 20:04:09 -0300

    added main menu with first user interaction

--- index.rb
+++ index.rb
@@ -12,6 +12,35 @@ class App
 
     def start
         Interaction.show_welcome_message
+        menu_selection = nil
+        # App loop
+        until menu_selection == 'q'
+            menu_selection = self.handleMenuLoop
+            case menu_selection
+                when 'd'
+                    #TODO display change dificulty menu            
+                when 'm'
+                    #TODO display change mode menu      
+                when 's'
+                    #TODO start game  
+            end          
+        end
+        Interaction.say('Bye! See you soon!')
+    end
+
+    def handleMenuLoop
+        menu_selection = nil
+        # Main menu loop
+        until menu_selection
+            Interaction.show_menu
+            menu_selection = Interaction.ask_input('d', 'm', 's', 'q')
+            if menu_selection
+                return menu_selection
+            else
+                Interaction.clear_console
+                Interaction.say('Wrong input, please try again...')
+            end
+        end
     end
 end
 
--- src/interaction.rb
+++ src/interaction.rb
@@ -4,4 +4,33 @@ module Interaction
         puts '  Do you want to play some tic-tac-toe?'
         puts '*---------------------------------------*'
     end
+        
+    def Interaction.show_menu
+        puts 'Enter:'
+        puts "  [s] to start game"
+        puts "  [d] to change difficulty (current difficulty: '#{ DIFFICULTYS[$current_dificulty] }')"
+        puts "  [m] to change game mode (current mode: '#{ MODES[$current_mode] }')"
+        puts "  [q] to quit"
+    end
+
+    def Interaction.ask_input(*valids)
+        input = gets.chomp
+        if valids.include?(input)
+            return input
+        else
+            return nil
+        end
+    end
+
+    def Interaction.clear_console
+        if RUBY_PLATFORM =~ /win32|win64|\.NET|windows|cygwin|mingw32/i
+            system('cls')
+        else
+            system('clear')
+        end
+    end
+
+    def Interaction.say(message)
+        puts message
+    end
 end
